8f52303e59961d14d0190b52b04a2bae7e792823
LeeMinWoo <minwooism2224@gmail.com>
2025-05-15 19:43:55 +0900

    Merge JungSangHwa

--- flutter_capston2025/lib/main.dart
+++ flutter_capston2025/lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
