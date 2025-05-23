67cfc64d277431b4ec2a5bf31877506e7ffa8383
jithhuP <107339114+jithhuP@users.noreply.github.com>
2025-05-23 21:11:44 +0900

    로그인 버튼 bottom 추가 및 메인 수정

--- flutter_capston2025/lib/pages/login_page.dart
+++ flutter_capston2025/lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'settings_page.dart';
+
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
